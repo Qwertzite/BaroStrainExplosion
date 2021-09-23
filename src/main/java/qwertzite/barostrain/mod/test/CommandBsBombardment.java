69580e5cf0bc95fb47997bf7ec24e3719061b3c4
qwertzite <qwertzite@gmail.com>
2021-09-23 17:04:24 +0900

    multiple explosion test. 途中

--- src/main/java/qwertzite/barostrain/mod/test/CommandBsBombardment.java
+++ src/main/java/qwertzite/barostrain/mod/test/CommandBsBombardment.java
@@ -5,6 +5,9 @@ import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.tileentity.CommandBlockBaseLogic;
+import net.minecraft.util.text.TextComponentString;
+import qwertzite.barostrain.core.BsExplosions;
+import qwertzite.barostrain.mod.CommandExplosion;
 
 public class CommandBsBombardment extends CommandBase {
 
@@ -23,12 +26,26 @@ public class CommandBsBombardment extends CommandBase {
 
 	@Override
 	public String getUsage(ICommandSender sender) {
-		return "/bsMulti x1 z1 x2 z2 p s";
+		return "/bsMulti x1 z1 x2 z2 h p s\n"
+				+ "h: margin from surface\n"
+				+ "p: probability per 1 m^2. clamped to [0, inf)\n"
+				+ "s: strength";
 	}
 
 	@Override
 	public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
 		// TODO Auto-generated method stub
+		double x1 = CommandExplosion.parseDouble(args[0]);
+		double z1 = CommandExplosion.parseDouble(args[1]);
+		double x2 = CommandExplosion.parseDouble(args[2]);
+		double z2 = CommandExplosion.parseDouble(args[3]);
+		double h = CommandExplosion.parseDouble(args[4]);
+		double p = CommandExplosion.parseDouble(args[5]);
+		double s = CommandExplosion.parseDouble(args[6]);
+		sender.sendMessage(new TextComponentString(String.format("Executing multiple explosions.")));
+		
+		
+//		BsExplosions.explode(sender.getEntityWorld(), null, x, y, z, (float) s);
 		
 	}
 
